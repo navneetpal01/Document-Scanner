9ccd6550bf598ca950c065494773c0a8fe512e5f
navneet <navneetpalgithub@gmail.com>
2024-06-18 10:44:51 +0530

    Mars

--- app/src/main/java/com/example/document_scanner/MainActivity.kt
+++ app/src/main/java/com/example/document_scanner/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.example.document_scanner
 
+import android.app.Activity
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.document_scanner.ui.theme.DocumentScannerTheme
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.SCANNER_MODE_FULL
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanning
+import com.google.mlkit.vision.documentscanner.GmsDocumentScanningResult
 
 
 class MainActivity : ComponentActivity() {
@@ -26,10 +47,66 @@ class MainActivity : ComponentActivity() {
             DocumentScannerTheme {
                 val options = GmsDocumentScannerOptions.Builder()
                     .setGalleryImportAllowed(false)
-                    .setResultFormats(RESULT_FORMAT_JPEG,RESULT_FORMAT_PDF)
-                    .setPageLimit()
+                    .setResultFormats(RESULT_FORMAT_JPEG, RESULT_FORMAT_PDF)
+                    .setPageLimit(2)
+                    .setScannerMode(SCANNER_MODE_FULL)
+                    .build()
+
+                var selectedImageUri by remember {
+                    mutableStateOf(Uri.EMPTY)
+                }
+
+                val scanner = GmsDocumentScanning.getClient(options)
+
+                val scannerLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.StartIntentSenderForResult()
+                ) { result ->
+
+                    if (result.resultCode == RESULT_OK) {
+                        val scanningResult =
+                            GmsDocumentScanningResult.fromActivityResultIntent(result.data)
+                        scanningResult?.pages?.let { pages ->
+                            for (page in pages) {
+                                selectedImageUri = pages[0].imageUri
+
+                            }
+                        }
+                    }
+                }
+
+                val activity = LocalContext.current as Activity
+
+
+                Column {
+
+                    AsyncImage(
+                        model = selectedImageUri,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(200.dp)
+                    )
+
+                    Button(
+                        onClick = {
+                            scanner.getStartScanIntent(activity)
+                                .addOnSuccessListener { intentSender ->
+                                    scannerLauncher.launch(
+                                        IntentSenderRequest
+                                            .Builder(intentSender)
+                                            .build()
+                                    )
+                                }
+                        }
+                    ) {
+                        Text(text = "Start scanning")
+                    }
+
+
+                }
+
+
             }
         }
     }
-
 }
