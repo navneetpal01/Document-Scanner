db309cb333378a57b8e3b2864d8bdd6c48bfbd7c
navneet <navneetpalgithub@gmail.com>
2024-06-18 10:06:05 +0530

    Mars

--- app/src/main/java/com/example/document_scanner/MainActivity.kt
+++ app/src/main/java/com/example/document_scanner/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.document_scanner.ui.theme.DocumentScannerTheme
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_JPEG
+import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions.RESULT_FORMAT_PDF
 
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +24,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DocumentScannerTheme {
-
+                val options = GmsDocumentScannerOptions.Builder()
+                    .setGalleryImportAllowed(false)
+                    .setResultFormats(RESULT_FORMAT_JPEG,RESULT_FORMAT_PDF)
+                    .setPageLimit()
             }
         }
     }
